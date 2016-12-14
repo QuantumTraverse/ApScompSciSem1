494a8b391bf37af7ab945f9db82f9b543c778975
QuantumTraverse <BartschR0439@sduhsd.lan>
2016-12-14 14:35:14 -0800

    changes 14.Dec

--- APLesson_09/divisor.java
+++ APLesson_09/divisor.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 public class divisor
 {
+	static Scanner kb = new Scanner(System.in);
 	static Random rand = new Random();
 	public static void main (String[]args)
 	{
@@ -10,13 +11,14 @@ public class divisor
 	printArray(randomnum);
 	System.out.println("Please enter a number: ");
 	int divisor = kb.nextInt();
+	System.out.println("There are " + divisability(randomnum, divisor) + " divisible by " + divisor + " in the Array.");
 	}
 	public static void fillArray(int[][] randomnum)
 	{
 		for(int i =0; i < randomnum.length; i++)
 		{
 			for(int j =0; j < randomnum.length; j++)
-				randomnum [i][j] = rand.nextInt(10);
+				randomnum [i][j] = 1+rand.nextInt(100);
 		}
 	}
 	public static void printArray(int[][] randomnum)
@@ -28,14 +30,14 @@ public class divisor
 			System.out.print("\n");
 		}
 	}
-	public static int divisability (int[][] randomnum)
+	public static int divisability (int[][] randomnum, int divisor)
 	{
 		int count = 0;
 		for(int i =0; i < randomnum.length; i++)
 		{
 			for(int j =0; j < randomnum.length; j++)
 			{
-				if (randomnum[i][j]%2 == 0)
+				if (randomnum[i][j]%divisor == 0)
 					count++;
 			}
 		}
---
+++ APLesson_11_1/gunGame.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class gunGame
+{
+	static Scanner kb = new Scanner(System.in);
+	static int bulletCount;
+	public static void main (String[]args)
+	{
+	}
+}

85c0b20d893beadc3c9f0320e3b858aa39f1b644
QuantumTraverse <BartschR0439@sduhsd.lan>
2016-12-16 14:34:07 -0800

    changes 16.Dec

---
+++ APLesson_11_1/gameHealth.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.util.Random;
+public class gameHealth
+{
+	static Random rand = new Random();
+	static Scanner kb = new Scanner(System.in);
+	static int healthCount;
+	static int healthLoad = 6;
+	static String [] health;
+	public static void main (String[]args)
+	{
+		String turn = "";
+		int damage = 0;
+		int amount = 0;
+		int healthCount = 6;
+		health = new String[healthLoad];
+		
+		while(!(turn.equals("Q"))|| healthCount > 0)
+		{
+			System.out.println("Your Turn. Hit Enter when Ready: ");
+			turn = kb.next();
+			damage = rand.nextInt(2);
+			amount = rand.nextInt(6);
+			System.out.println(takeDamage(damage, amount));
+			printClip();
+		}
+		System.out.println("You died... :(");
+	}
+	public static void takeDamage(int dmg, int amnt)
+	{
+		if (dmg ==1)
+		{
+			healthCount -= amnt;
+			return "Taking amnt damage!";
+		}
+		else
+		{
+			if (healthCount + amnt < healthLoad)
+				healthCount += amnt;
+			else
+				healthCount = healthLoad;
+		}
+		return "Power up " + amnt + "!"
+	}
+	public static void printClip()
+	{
+		String output = "Health: \t"
+		for(int i +0; i < healthLoad; i ++)
+		{
+			if (i < healthCount)
+				health[i] = " @ "
+			else
+				health[i] = " []"
+			output = output + health[i];
+		}
+	}
+}
